5a1545e7e041e55aee6a2ddccaf36de59229d0ed
KotoriYousan <kurosawa1997@gmail.com>
2016-11-11 08:59:41 +0800

    Update HotelDataService.java

--- Client/src/dataservice/hoteldataservice/HotelDataService.java
+++ Client/src/dataservice/hoteldataservice/HotelDataService.java
@@ -6,12 +6,61 @@ import java.util.ArrayList;
 import po.HotelPO;
 
 public interface HotelDataService {
+	
+	/**
+	 * 
+	 * @param hpo
+	 * @throws RemoteException
+	 */
 	public void delete(HotelPO hpo) throws RemoteException; 
+	
+	/**
+	 * 
+	 * @param hpo
+	 * @throws RemoteException
+	 */
 	public void insert(HotelPO hpo) throws RemoteException;
+	
+	/**
+	 * 
+	 * @param hpo
+	 * @throws RemoteException
+	 */
 	public void update(HotelPO hpo) throws RemoteException;
+	
+	/**
+	 * 
+	 * @param name
+	 * @return 根据名称查找酒店
+	 * @throws RemoteException
+	 */
 	public HotelPO find(String name) throws RemoteException;
+	
+	/**
+	 * 
+	 * @param field
+	 * @param value
+	 * @return 根据范围和值查找酒店
+	 * @throws RemoteException
+	 */
 	public ArrayList<HotelPO> finds(String field,String value) throws RemoteException;
+	
+	/**
+	 * 
+	 * @return 浏览所有酒店的列表
+	 * @throws RemoteException
+	 */
 	public ArrayList<HotelPO> finds() throws RemoteException;
+	
+	/**
+	 * 
+	 * @throws RemoteException
+	 */
 	public void init() throws RemoteException;
+	
+	/**
+	 * 
+	 * @throws RemoteException
+	 */
 	public void finish() throws RemoteException;
 }
