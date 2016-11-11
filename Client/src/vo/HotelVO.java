316b963b0e1a9cf7f6b3735e1e3da191b6ebb7ec
KotoriYousan <kurosawa1997@gmail.com>
2016-11-11 09:03:13 +0800

    Update HotelVO.java

--- Client/src/vo/HotelVO.java
+++ Client/src/vo/HotelVO.java
@@ -29,39 +29,75 @@ public class HotelVO implements Serializable{
 		rating =r;
 	}
 	
+	/**
+	 * 
+	 * @return 获得酒店ID
+	 */
 	public int getHotelID(){
 		return hotelID;
 		
 	}
 	
+	/**
+	 * 
+	 * @return 获得酒店名称
+	 */
 	public String getHotelName(){
 		return hotelName;
 	}
 	
+	/**
+	 * 
+	 * @return 获得酒店地址
+	 */
 	public String getHotelAddress(){
 		return hotelAddress;
 	}
 	
+	/**
+	 * 
+	 * @return 获得酒店商圈
+	 */
 	public String getBusinessArea(){
 		return businessArea;
 	}
 	
+	/**
+	 * 
+	 * @return 获得酒店简介
+	 */
 	public String getHotelDescription(){
 		return hotelDescription;
 	}
 	
+	/**
+	 * 
+	 * @return 获得酒店星级
+	 */
 	public int getStarLevel(){
 		return starLevel;
 	}
 	
+	/**
+	 * 
+	 * @return 获得房间号
+	 */
 	public int getRoomNumber(){
 		return roomNumber;
 	}
 	
+	/**
+	 * 
+	 * @return 获得酒店房间列表
+	 */
 	public ArrayList<RoomVO> getRooms(){
 		return wholeRooms;
 	}
 	
+	/**
+	 * 
+	 * @return 获得酒店评级
+	 */
 	public int getRating(){
 		return rating;
 	}
