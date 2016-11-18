d8f2af41a567075ade5fd0ada096fe3aab333583
KotoriYousan <kurosawa1997@gmail.com>
2016-11-19 01:25:33 +0800

    更新hotel主界面跳转

--- Client/src/presentation/mainui/Hotel_start.java
+++ Client/src/presentation/mainui/Hotel_start.java
@@ -22,7 +22,7 @@ public class Hotel_start extends Application {
 		}
 	}
 
-//	public static void main(String[] args) {
-//		launch(args);
-//	}
+	public static void main(String[] args) {
+		launch(args);
+	}
 }
